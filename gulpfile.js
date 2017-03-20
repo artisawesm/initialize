1b70f63a77978d89e3383f50f1a65aaab9f80c31
artisawesm <asd.artsandiego@gmail.com>
2017-03-20 14:14:14 +0800

    Testing directory watch for gulp - NOT WORKING -

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 
 var path = {
-	jsLib: './src/assets/script/lib/*.js'
+	jsLib: './src/assets/script/lib/'
 };
 
 gulp.task('initialize', function() {
@@ -11,4 +11,8 @@ gulp.task('initialize', function() {
 	.pipe(concat('lib.min.js'))
 	.pipe(gulp.dest('./dist/assets/script/lib'));
 
+});
+
+gulp.task('serve', function() {
+	gulp.watch(path.jsLib, ['initialize']);
 });
